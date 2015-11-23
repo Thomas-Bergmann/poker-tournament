7eebf75c82db570869f237a838ebea7e7bcdf0c6
Thomas Bergmann <t.bergmann@intershop.de>
2015-11-24 00:15:37 +0100

    move runtime dependencies to offlinepoker project

--- de.hatoka.account/src/test/java/de/hatoka/account/internal/dao/DerbyEntityManagerRule.java
+++ de.hatoka.account/src/test/java/de/hatoka/account/internal/dao/DerbyEntityManagerRule.java
@@ -1,18 +1,11 @@
 package de.hatoka.account.internal.dao;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import org.apache.derby.jdbc.EmbeddedDataSource;
-import org.apache.naming.java.javaURLContextFactory;
 import org.junit.rules.ExternalResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
@@ -22,37 +15,6 @@ import de.hatoka.common.capi.dao.TransactionProvider;
 
 public class DerbyEntityManagerRule extends ExternalResource implements EntityManagerProvider
 {
-    private static void initContext()
-    {
-        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, javaURLContextFactory.class.getName());
-        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
-
-        try
-        {
-            InitialContext ic = new InitialContext();
-            ic.createSubcontext("java:");
-            ic.createSubcontext("java:comp");
-            ic.createSubcontext("java:comp/env");
-            ic.createSubcontext("java:comp/env/jdbc");
-
-            EmbeddedDataSource ds = new EmbeddedDataSource();
-            ds.setDatabaseName("build/derby-test-db");
-            // tell Derby to create the database if it does not already exist
-            ds.setCreateDatabase("create");
-            ic.bind("java:comp/env/jdbc/AccountTestDS", ds);
-        }
-        catch(NamingException e)
-        {
-            LOGGER.error("Can't bind test database.", e);
-        }
-    }
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DerbyEntityManagerRule.class);
-    static
-    {
-        initContext();
-    }
-
     private EntityManagerFactory emf;
     private EntityManager em;
 
