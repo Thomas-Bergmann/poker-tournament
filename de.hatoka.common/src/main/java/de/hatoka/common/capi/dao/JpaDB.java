b2e1dd0196c61ea96ad691fce07ef96fcc721e6b
Thomas Bergmann <t.bergmann@intershop.de>
2015-11-23 23:46:44 +0100

    move persistence mapping to env.

--- de.hatoka.common/src/main/java/de/hatoka/common/capi/dao/JpaDB.java
+++ de.hatoka.common/src/main/java/de/hatoka/common/capi/dao/JpaDB.java
@@ -2,6 +2,7 @@ package de.hatoka.common.capi.dao;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,9 +15,16 @@ public class JpaDB
 
     public JpaDB(String persistenceUnitName)
     {
-        Map<String, Object> addedOrOverridenProperties = new HashMap<>();
+        Map<String, Object> addedOrOverridenProperties = new HashMap<String, Object>();
         // Let's suppose we are using Hibernate as JPA provider
         addedOrOverridenProperties.put("hibernate.show_sql", true);
+        for (Entry<String, String> systemEntry : System.getenv().entrySet())
+        {
+            if (systemEntry.getKey().startsWith("javax.persistence.jdbc"))
+            {
+                addedOrOverridenProperties.put(systemEntry.getKey(), systemEntry.getValue());
+            }
+        }
 
         emf = Persistence.createEntityManagerFactory(persistenceUnitName);
         em = emf.createEntityManager();
